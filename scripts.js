6f31815b53af98773ae83d4987f5d36a2832cc41
Sravankumar369 <145766475+Sravankumar369@users.noreply.github.com>
2024-07-09 10:30:23 +0530

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,3 +1,35 @@
+/*console.clear();
+
+gsap.registerPlugin(ScrollTrigger);
+
+window.addEventListener("load", () => {
+  gsap.timeline({
+    scrollTrigger: {
+      trigger: ".wrapper1",
+      start: "top top",
+      end: "+=150%",
+      pin: true,
+      scrub: true,
+      markers: true
+    }
+  })
+  .to("img", {
+    scale: 2,
+    z: 350,
+    transformOrigin: "center center",
+    ease: "power1.inOut"
+  })
+  .to(
+    ".main_container",
+    {
+      scale: 1.1,
+      transformOrigin: "center center",
+      ease: "power1.inOut"
+    },
+    "<"
+  );
+});
+*/ 
 /* go to top button*/
 // Get the button
 let goToTopBtn = document.getElementById("goToTopBtn");
